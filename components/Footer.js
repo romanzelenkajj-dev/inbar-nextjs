612c7d24aea3ede5592505078c8852f7c2e730b1
romanzelenkajj-dev <romanzelenkajj@gmail.com>
2026-03-15 20:16:38 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -10,8 +10,6 @@ export default function Footer() {
             <div className="footer-logo">
               <img src="https://inbar.sk/app/uploads/2020/03/INBAR-LOGO-WEB.png" alt="InBar Magazine" height="44" />
             </div>
-            <p className="footer-tagline">Bar &amp; Restaurant Magazine</p>
-            <p className="footer-desc">Slovensky magazin o baroch, restauraciach a zivotnom style. Drinking, Dining, Living.</p>
             <div className="footer-social">
               <a href="https://instagram.com/inbarsk" target="_blank" rel="noreferrer" aria-label="Instagram">
                 <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
@@ -23,7 +21,7 @@ export default function Footer() {
           </div>
 
           <div>
-            <p className="footer-nav-title">Kategorie</p>
+            <p className="footer-nav-title">Kategórie</p>
             <ul className="footer-nav-list">
               <li><Link href="/category/drinking">Drinking</Link></li>
               <li><Link href="/category/dining">Dining</Link></li>
@@ -34,7 +32,7 @@ export default function Footer() {
           <div>
             <p className="footer-nav-title">Kontakt</p>
             <ul className="footer-nav-list">
-              <li><Link href="/o-nas">O nas</Link></li>
+              <li><Link href="/o-nas">O nás</Link></li>
               <li><Link href="/kontakt">Kontakt</Link></li>
               <li><Link href="/inzercia">Inzercia</Link></li>
             </ul>
@@ -42,9 +40,9 @@ export default function Footer() {
         </div>
 
         <div className="footer-bottom">
-          <span>© {year} InBar Magazine. Vsetky prava vyhradene.</span>
+          <span>© {year} InBar Magazine. Všetky práva vyhradené.</span>
           <div className="footer-bottom-links">
-            <Link href="/ochrana-sukromia">Ochrana sukromia</Link>
+            <Link href="/ochrana-sukromia">Ochrana súkromia</Link>
             <Link href="/cookies">Cookies</Link>
           </div>
         </div>
