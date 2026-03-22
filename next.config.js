e4b40048031e1e543e4480052f356636958d1c97
Roman Zelenka <romanzelenkajj@gmail.com>
2026-03-22 18:49:27 +0000

    Rewrite /mediakit to /mediakit.html

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/mediakit',
+        destination: '/mediakit.html',
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
